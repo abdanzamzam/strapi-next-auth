5f1bb00222c69fd76e25d1e5bba1644373236664
Abdan Zam Zam Ramadhan <abdan.zamzamramadhan@ai.astra.co.id>
2023-01-18 07:43:09 +0700

    [CLIENT] Development login, register & auto redirect to dashboard

---
+++ client/pages/dashboard.js
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        Dashboard
+      </div>
+    </>
+  );
+}
--- client/pages/login.js
+++ client/pages/login.js
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { useState } from "react";
 import nookies from "nookies";
+import Router from "next/router";
 
 export async function getServerSideProps(ctx) {
   const cookies = nookies.get(ctx);
@@ -17,11 +19,9 @@ export async function getServerSideProps(ctx) {
   };
 }
 
-export default function Register() {
+export default function Login() {
   const [field, setField] = useState({});
   const [progress, setProgress] = useState(false);
-  const [success, setSuccess] = useState(false);
-  const [messages, setMessages] = useState("");
 
   function setValue(e) {
     const target = e.target;
@@ -34,39 +34,23 @@ export default function Register() {
     });
   }
 
-  async function doRegister(e) {
+  async function doLogin(e) {
     e.preventDefault();
     setProgress(true);
 
-    const req = await fetch(
-      `${process.env.NEXT_PUBLIC_URL}/api/auth/local/register`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(field)
-      }
-    );
+    const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/auth/local`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(field)
+    });
 
     const res = await req.json();
 
     if (res.jwt) {
-      setSuccess(true);
-      e.target.reset();
-      setField({});
-
-      setTimeout(() => {
-        setSuccess(false);
-      }, 3000);
-    } else {
-      setMessages(res.error.message);
-      setSuccess(true);
-
-      setTimeout(() => {
-        setSuccess(false);
-        setMessages("");
-      }, 3000);
+      nookies.set(null, "token", res.jwt);
+      Router.replace("/dashboard");
     }
 
     setProgress(false);
@@ -76,43 +60,19 @@ export default function Register() {
     <>
       <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
         <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-          <h1 className="font-bold text-center text-2xl mb-5">Register</h1>
-          {success && (
-            <>
-              {messages ? (
-                <div className="bg-red-500 shadow text-white rounded mb-4 px-4 py-3">
-                  {messages}
-                </div>
-              ) : (
-                <div className="bg-green-500 shadow text-white rounded mb-4 px-4 py-3">
-                  Congratulations! Your account has been registered.
-                </div>
-              )}
-            </>
-          )}
+          <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1>
           <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-            <form onSubmit={doRegister} className="px-5 py-7 relative">
+            <form onSubmit={doLogin} className="px-5 py-7 relative">
               {progress && (
                 <div className="absolute inset-0 z-10 bg-white/50" />
               )}
-              <label className="font-semibold text-sm text-gray-600 pb-1 block">
-                Username
-              </label>
-              <input
-                type="text"
-                className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                // value={field.username}
-                name="username"
-                onChange={setValue}
-              />
               <label className="font-semibold text-sm text-gray-600 pb-1 block">
                 E-mail
               </label>
               <input
                 type="text"
                 className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                // value={field.email}
-                name="email"
+                name="identifier"
                 onChange={setValue}
               />
               <label className="font-semibold text-sm text-gray-600 pb-1 block">
@@ -121,7 +81,6 @@ export default function Register() {
               <input
                 type="password"
                 className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                // value={field.password}
                 name="password"
                 onChange={setValue}
               />
@@ -147,6 +106,23 @@ export default function Register() {
                 </svg>
               </button>
             </form>
+            <div className="p-5">
+              <a
+                type="submit"
+                className="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block"
+              >
+                Login with Github
+              </a>
+            </div>
+            <div className="py-5">
+              <div className="grid grid-cols-2 gap-1">
+                <Link href="/register">
+                  <p className="cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:text-blue-600 mx-5 px-5 py-4">
+                    Register
+                  </p>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
