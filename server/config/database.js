ab057b57659f486e0b1d872f460f08c3cad71445
Abdan Zam Zam Ramadhan <abdan.zamzamramadhan@ai.astra.co.id>
2023-01-19 10:27:40 +0700

    Setup environment for database using .env

--- server/config/database.js
+++ server/config/database.js
@@ -1,29 +1,13 @@
-const path = require("path");
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
-
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "103.236.201.56"),
+      host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "Server001Postgres!@#$%"),
-      // schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: false
-      // ssl: {
-      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-      // },
+      password: env("DATABASE_PASSWORD", "postgres"),
+      ssl: false,
     },
     debug: false,
   },
